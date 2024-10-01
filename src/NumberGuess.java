552dc0d67c9b7d4f2bd077da99d3cf08249f1f1f
TheItcor <Itcorsmail@gmail.com>
2024-10-01 19:57:15 +0300

    My first project on Java :3

---
+++ src/NumberGuess.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class NumberGuess {
+    public static void main(String[] args) {
+        int menuRun = 1;
+        Scanner scan = new Scanner(System.in);
+        while (menuRun == 1) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("Start game? (y/n)");
+            String answer = scan.next();
+            switch (answer) {
+                case "y":
+                    menuRun = 0;
+                    break;
+                case "n":
+                    System.exit(0);
+            }
+        }
+
+        System.out.println("Guess the number!");
+
+        int trueNumber = (int) (Math.random() * 100);
+        int gameRun = 1;
+        while (gameRun == 1) {
+            int guessNumber = scan.nextInt();
+
+            if (guessNumber == trueNumber) {
+                gameRun = 0;
+                System.out.printf("You win! The number is %d \n", trueNumber);
+            }
+            else if (guessNumber > trueNumber) {
+                System.out.println("Your's number is too big!");
+            }
+            else {
+                System.out.println("Your's number is too small!");
+            }
+
+        }
+    }
+}
